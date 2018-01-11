9b272ae0aaf4336db552e3a7b299de27e4138187
scott <scott.lan@taisys.com>
2018-01-11 20:17:42 +0800

    update code

--- CmdCompare.py
+++ CmdCompare.py
@@ -17,8 +17,12 @@ class CmdCompareCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         subprocess.Popen([str(get_location()), str(fileQueue[0]), str(fileQueue[1])])
 
+    def is_enabled(self):
+        return len(fileQueue) >= 2
+
 class PluginListener(sublime_plugin.EventListener):
     def on_activated(self, view = None):
+        print([view, view.file_name()])
         global fileQueue
         if view.file_name() is not None:
             fileQueue.append(view.file_name())
